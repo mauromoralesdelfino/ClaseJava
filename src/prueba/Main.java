6de01b15754c6fe12dbf2e338920ac1e89a10726
alumno <alumno>
2018-08-21 22:28:31 -0300

    hash

--- src/prueba/Main.java
+++ src/prueba/Main.java
@@ -8,17 +8,33 @@ public class Main
 		System.out.println("Bienvenidos!");
 		
 		
-		Persona p1 = new Persona(37762902,"Mauro","Morales Delfino");
+		//Persona p1 = new Persona(122,"Mauro","Morales Delfino");
+		Persona p1 = new Persona();
 		Persona p2 = new Persona();
-		p2.setDni(37762902);
+		//p2.setDni(122);
+		p1.setNombre("Mauro");
+		p1.setApellido("Morales Delfino");
 		p2.setNombre("Mauro");
 		p2.setApellido("Morales Delfino");
+		/*imprimir hashcode */
 		
-		
-		if(p1!= p2)
+		if(p1.equals(p2))
 		{
-			System.out.println(p1.toString());
-			System.out.println(p2.toString());
+		
+			System.out.println("iguales");
+			System.out.println(p1.hashCode());
+			System.out.println(p2.hashCode());
+		}
+		else
+		{
+			System.out.println("NONONO");
+		}
+		
+		if(p1.hashCode() == p2.hashCode())
+		{
+			System.out.println(p1.hashCode());
+			System.out.println("NONONO");
+			System.out.println(p2.hashCode());
 		}
 		
 	}
--- src/prueba/Persona.java
+++ src/prueba/Persona.java
@@ -2,12 +2,12 @@ package prueba;
 
 public class Persona 
 {
-  private int _dni;
+  private double _dni;
   private String _nombre;
   private String _apellido; 
   
   
-  public Persona(int dni,String nombre, String Apellido)
+  public Persona(double dni,String nombre, String Apellido)
   {
 	  _dni = dni;
 	  _nombre=nombre;
@@ -35,11 +35,11 @@ public class Persona
           this._apellido = apellido;
   }
   
-  public int getDni()
+  public double getDni()
   {
 	  return _dni;
   }
-  public void setDni(int dni)
+  public void setDni(double dni)
   {
 	  this._dni = dni;
   }
@@ -50,4 +50,69 @@ public class Persona
 	  return "Nombre: " + this._nombre + " Apellido: " + this._apellido + " DNI: " + this._dni;
   }
 
+ /* @Override
+  public boolean equals(Object obj)
+  {
+	  if(obj instanceof Persona)
+	  {
+		  Persona tmpPersona = (Persona)obj;
+		  if (this._nombre.equals(tmpPersona._nombre) && this._apellido.equals(tmpPersona._apellido) &&
+				  this._dni == tmpPersona._dni) 
+		  		{ return true; } 
+		  else { return false; }
+	  } 
+	  else 
+	  	{ return false; }
+				 
+	  
+	  return true;
+  }*/
+  
+  @Override
+  public boolean equals(Object obj)
+  {
+	  if(this == obj)
+	  {
+		  return true;
+		  
+	  }
+	  if(obj == null)
+	  {
+		  return false;
+	  }
+	  if(this.getClass() != obj.getClass())
+	  	{
+		  return false;
+	  	}
+	  
+	  Persona tmpPersona = (Persona) obj;
+	  //if(tmpPersona._dni != null)
+	  	//{
+		  if(this._dni == tmpPersona._dni)
+		  {
+			  return true;
+		  }
+	  	//}
+	  
+	  return false;
+	  
+  }
+  @Override
+  public int hashCode()
+  {
+	  final int key = 31;
+	  int numero =1;
+	  
+	  if(this._nombre != null)
+	  {
+		  numero += numero *  key * this._nombre.hashCode();
+	  }
+	  if(this._apellido != null)
+	  {
+		  numero += numero *  key * this._apellido.hashCode();
+	  }
+	 
+	  
+	  return numero;
+  }
 }
